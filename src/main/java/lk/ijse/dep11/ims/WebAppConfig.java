070b745221581864517a36b851a0fe4e24314f7f
Chamara Yaparathne <kanishkayaparathne@gmail.com>
2023-11-22 14:29:01 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/dep11/ims/WebAppConfig.java
@@ -0,0 +1,10 @@
+package lk.ijse.dep11.ims;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+public class WebAppConfig {
+}
